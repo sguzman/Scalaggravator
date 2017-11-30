355df6e67aed1171d899eb7ab4c9bb061ea57ef2
Salvador Guzman <guzmansalv@gmail.com>
2017-11-30 11:46:03 -0800

    made login method

--- src/main/scala/com/github/sguzman/scala/aggrava/tor/Main.scala
+++ src/main/scala/com/github/sguzman/scala/aggrava/tor/Main.scala
@@ -2,9 +2,18 @@ package com.github.sguzman.scala.aggrava.tor
 
 import com.github.sguzman.scala.aggrava.tor.args.Args
 
+import scalaj.http.Http
+
 object Main {
   def main(args: Array[String]): Unit = {
     val argv = Args(args)
-    println(argv)
+    lazy val lgn = login
+
+  }
+
+  def login = {
+    println("login")
+    val login = Http(url = "https://my.sa.ucsb.edu/gold/login.aspx")
+    login
   }
 }
