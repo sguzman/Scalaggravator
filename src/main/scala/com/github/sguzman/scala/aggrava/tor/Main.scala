df128f9df08f83804f82ba82dca9bbb2f736cb7a
Salvador Guzman <guzmansalv@gmail.com>
2017-12-01 09:54:09 -0800

    remove empty department

--- src/main/scala/com/github/sguzman/scala/aggrava/tor/Main.scala
+++ src/main/scala/com/github/sguzman/scala/aggrava/tor/Main.scala
@@ -1,13 +1,5 @@
 package com.github.sguzman.scala.aggrava.tor
 
-import java.net.HttpCookie
-
-import net.ruippeixotog.scalascraper.browser.JsoupBrowser
-import net.ruippeixotog.scalascraper.dsl.DSL.Extract._
-import net.ruippeixotog.scalascraper.dsl.DSL._
-
-import scalaj.http.{Http, HttpResponse}
-
 object Main {
   def main(args: Array[String]): Unit = {
     val argv = Args(args)
@@ -17,8 +9,10 @@ object Main {
     println(quarters)
 
     val logins = quarters.map(_ => Login(argv))
-    val departments = quarters.zip(logins).map(a => a._1 -> Departments(a._2))
+    val departments = quarters.zip(logins).map(a => a._1 -> Departments(a._2).filter(_.nonEmpty))
     println(departments)
+
+
   }
 }
 
