a97ac49fd1f1c040745c0fbef26e1bb4fe674ce4
Salvador Guzman <guzmansalv@gmail.com>
2017-12-01 10:05:21 -0800

    generate (quarter, department) argument groupings

--- src/main/scala/com/github/sguzman/scala/aggrava/tor/Main.scala
+++ src/main/scala/com/github/sguzman/scala/aggrava/tor/Main.scala
@@ -8,11 +8,12 @@ object Main {
     val quarters = Quarters(lgn)
     println(quarters)
 
-    val logins = quarters.map(_ => Login(argv))
-    val departments = quarters.zip(logins).map(a => a._1 -> Departments(a._2).filter(_.nonEmpty))
-    println(departments)
+    val logins = quarters map (_ => Login(argv))
+    val deptsByQuarts = quarters zip logins map (a => a._1 -> Departments(a._2).filter (_.nonEmpty))
+    println(deptsByQuarts)
 
-
+    val arguments = deptsByQuarts flatMap (t => t._2 map ((t._1, _)))
+    println(arguments)
   }
 }
 
