d854b133647f159c212c8e7b064cae791d819275
Salvador Guzman <guzmansalv@gmail.com>
2017-12-01 06:35:59 -0800

    adde departments code

--- src/main/scala/com/github/sguzman/scala/aggrava/tor/Main.scala
+++ src/main/scala/com/github/sguzman/scala/aggrava/tor/Main.scala
@@ -2,11 +2,11 @@ package com.github.sguzman.scala.aggrava.tor
 
 import java.net.HttpCookie
 
-import net.ruippeixotog.scalascraper.browser.{Browser, JsoupBrowser}
+import net.ruippeixotog.scalascraper.browser.JsoupBrowser
 import net.ruippeixotog.scalascraper.dsl.DSL.Extract._
 import net.ruippeixotog.scalascraper.dsl.DSL._
 
-import scalaj.http.{Http, HttpRequest, HttpResponse}
+import scalaj.http.{Http, HttpResponse}
 
 object Main {
   def main(args: Array[String]): Unit = {
@@ -17,9 +17,25 @@ object Main {
     println(quarters)
 
     val logins = quarters.map(_ => Login(argv))
-    val departments = quarters.zip(logins).map(a => a._1 -> Quar)
+    val departments = quarters.zip(logins).map(a => a._1 -> Departments(a._2))
+    println(departments)
   }
 }
 
+object Departments {
+  private def get(cookies: IndexedSeq[HttpCookie]): HttpResponse[String] = {
+    val url = "https://my.sa.ucsb.edu/gold/BasicFindCourses.aspx"
+    Http(url)
+      .header("Cookie", cookies.mkString("; ")).asString
+  }
+
+  def apply(response: HttpResponse[String]): List[String] = {
+    val id = "pageContent_subjectAreaDropDown"
+    val basicFindCoursesPage = get(response.cookies)
+    val doc = JsoupBrowser().parseString(basicFindCoursesPage.body)
+    val menu = doc >> elementList(s"#$id > option")
+    menu map (_ attr "value")
+  }
+}
 
 
