9ad0250c52b98e00342a88d8581580a51413a78f
Salvador Guzman <guzmansalv@gmail.com>
2017-12-01 05:49:26 -0800

    add a login method

--- src/main/scala/com/github/sguzman/scala/aggrava/tor/Main.scala
+++ src/main/scala/com/github/sguzman/scala/aggrava/tor/Main.scala
@@ -1,19 +1,47 @@
 package com.github.sguzman.scala.aggrava.tor
 
-import com.github.sguzman.scala.aggrava.tor.args.Args
+import java.net.URLEncoder
 
-import scalaj.http.Http
+import com.github.sguzman.scala.aggrava.tor.args.Args
+import net.ruippeixotog.scalascraper.browser.JsoupBrowser
+import net.ruippeixotog.scalascraper.dsl.DSL._
+import net.ruippeixotog.scalascraper.dsl.DSL.Extract._
+import net.ruippeixotog.scalascraper.dsl.DSL.Parse._
+
+import scalaj.http.{Http, HttpRequest}
 
 object Main {
   def main(args: Array[String]): Unit = {
     val argv = Args(args)
-    lazy val lgn = login
+    val lgn = login(argv)
 
   }
 
-  def login = {
+  def login(argv: Args): HttpRequest = {
     println("login")
-    val login = Http(url = "https://my.sa.ucsb.edu/gold/login.aspx")
-    login
+    val url = "https://my.sa.ucsb.edu/gold/login.aspx"
+    val resp = Http(url)
+
+    val soup = JsoupBrowser()
+    val doc = soup.parseString(resp.asString.body)
+    val hidden = doc >> elementList("""input[type="hidden"]""")
+    val hiddenVals = hidden.map(s => List(s.attr("name"), s.attr("value")))
+    val inputVals = if (argv.old) List(
+      List("ctl00%24pageContent%24PermPinLogin%24userNameText", argv.user),
+      List("ctl00%24pageContent%24PermPinLogin%24passwordText", argv.pass),
+      List("ctl00%24pageContent%24PermPinLogin%24loginButton.x", "0"),
+      List("ctl00%24pageContent%24PermPinLogin%24loginButton.y", "0")
+    ) else List(
+      List("ctl00%24pageContent%24userNameText", argv.user),
+      List("ctl00%24pageContent%24passwordText", argv.pass),
+      List("ctl00%24pageContent%24loginButton.x", "0"),
+      List("ctl00%24pageContent%24loginButton.y", "0")
+    )
+
+    val bodyPairs = hiddenVals ++ inputVals
+    val form = bodyPairs.map(s => s"${s.head}=${URLEncoder.encode(s(1), UTF_8.toString)}").mkString("&")
+
+    val request = resp.postData(form)
+    request
   }
 }
