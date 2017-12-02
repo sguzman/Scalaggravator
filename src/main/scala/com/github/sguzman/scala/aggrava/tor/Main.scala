5e8c9c8c5e14787d0bed085e18d3d1482a07ce49
Salvador Guzman <guzmansalv@gmail.com>
2017-12-01 22:12:38 -0800

    print out by line

--- src/main/scala/com/github/sguzman/scala/aggrava/tor/Main.scala
+++ src/main/scala/com/github/sguzman/scala/aggrava/tor/Main.scala
@@ -6,26 +6,27 @@ object Main {
     val lgn = Login(argv)
 
     val quarters = Quarters(lgn)
-    println(quarters)
+    quarters foreach println
 
     val logins = quarters.par map (_ => Login(argv))
     val deptsByQuarts =
       quarters zip logins map (a => a._1 -> Departments(a._2)
         .map(_.trim)
         .filter(_.nonEmpty))
-    println(deptsByQuarts)
+    deptsByQuarts foreach println
 
     val arguments = if (argv.debug)
       deptsByQuarts.filter(_._1 == "20174").flatMap(t => t._2.map((t._1, _)))
     else
       deptsByQuarts flatMap (t => t._2 map ((t._1, _)))
-    println(arguments)
+    arguments foreach println
 
     val loginsForArgs = arguments.par map (_ => Login(argv))
     val results = arguments.zip(loginsForArgs).par
       .map(t => (t._1._1, t._1._2, t._2)).map(t => Search(t._1, t._2, t._3))
 
-    println(results)
+    val results = searchArgs.map(t => Search(t._1, t._2, t._3))
+    results foreach println
   }
 }
 
