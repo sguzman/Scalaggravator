e41613b47f2b49630d360741bfaa5c9f4f7db89b
Salvador Guzman <guzmansalv@gmail.com>
2017-12-02 18:29:29 -0800

    combine all arguments together

--- src/main/scala/com/github/sguzman/scala/aggrava/tor/Main.scala
+++ src/main/scala/com/github/sguzman/scala/aggrava/tor/Main.scala
@@ -29,6 +29,10 @@ object Main {
 
     val parsed = resultResponses.par map Split.apply
     parsed foreach println
+
+
+    val quartDeptCourses = arguments.zip(parsed).map(t => (t._1._1, t._1._2, t._2))
+    quartDeptCourses foreach println
   }
 }
 
