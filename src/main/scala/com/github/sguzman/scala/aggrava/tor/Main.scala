e80c94632ead75455fe8278058ac703e64373e19
Salvador Guzman <guzmansalv@gmail.com>
2017-12-04 11:59:44 -0800

    simple filtering facilities

--- src/main/scala/com/github/sguzman/scala/aggrava/tor/Main.scala
+++ src/main/scala/com/github/sguzman/scala/aggrava/tor/Main.scala
@@ -37,6 +37,24 @@ object Main {
   }
 }
 
+object Filter {
+  private def byQuarter(model: List[Quarter], quarter: String): List[Quarter] =
+    model.filter(_.term.toLowerCase.contains(quarter.toLowerCase))
 
+  def byQuarterJson(model: List[Quarter], quarter: String): String =
+    new GsonBuilder().create.toJson(byQuarter(model, quarter).toArray)
 
+  private def byDepartment(model: List[Quarter], quarter: String, department: String): List[Quarter] =
+    byQuarter(model, quarter)
+      .filter(_.departments.department.toLowerCase.contains(department.toLowerCase))
 
+  def byDepartmentJson(model: List[Quarter], quarter: String, department: String): String =
+    new GsonBuilder().create.toJson(byDepartment(model, quarter, department).toArray)
+
+  private def byCourse(model: List[Quarter], quarter: String, department: String, courseName: String): List[Quarter] =
+    byDepartment(model, quarter, department)
+    .filter(_.departments.courses.course.toLowerCase.contains(courseName.toLowerCase))
+
+  def byCourseJson(model: List[Quarter], quarter: String, department: String, courseName: String): String =
+    new GsonBuilder().create.toJson(byCourse(model, quarter, department, courseName).toArray)
+}
