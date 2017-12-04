1cff16d86ff8d6ae22b87e82ea1074eed93a8750
Salvador Guzman <guzmansalv@gmail.com>
2017-12-04 08:53:24 -0800

    add errorhandler to catch timeout exception

--- src/main/scala/com/github/sguzman/scala/aggrava/tor/Search.scala
+++ src/main/scala/com/github/sguzman/scala/aggrava/tor/Search.scala
@@ -7,28 +7,39 @@ import net.ruippeixotog.scalascraper.browser.JsoupBrowser
 import net.ruippeixotog.scalascraper.dsl.DSL.Extract._
 import net.ruippeixotog.scalascraper.dsl.DSL._
 
+import scala.util.{Failure, Success}
 import scalaj.http.{Http, HttpRequest, HttpResponse}
 
 object Search {
-  def apply(quarter: String, department: String, response: HttpResponse[String]): HttpResponse[String] = {
-    val basicCoursePageRequest = basicCoursePage(response.cookies)
+  def apply(quarter: String,
+            department: String,
+            response: HttpResponse[String],
+            errorHandler: Any => Unit = Console.err.println): HttpResponse[String] = {
+    util.Try({
+      val basicCoursePageRequest = basicCoursePage(response.cookies)
 
-    val doc = JsoupBrowser().parseString(basicCoursePageRequest.asString.body)
-    val hidden = doc >> elementList("""input[type="hidden"]""")
-    val hiddenVals = hidden.map(s => List(s.attr("name"), s.attr("value")))
-    val inputVals = List(
-      List("ctl00%24pageContent%24quarterDropDown", quarter),
-      List("ctl00%24pageContent%24subjectAreaDropDown", department),
-      List("ctl00%24pageContent%24searchButton.x", "0"),
-      List("ctl00%24pageContent%24searchButton.y", "0")
-    )
+      val doc = JsoupBrowser().parseString(basicCoursePageRequest.asString.body)
+      val hidden = doc >> elementList("""input[type="hidden"]""")
+      val hiddenVals = hidden.map(s => List(s.attr("name"), s.attr("value")))
+      val inputVals = List(
+        List("ctl00%24pageContent%24quarterDropDown", quarter),
+        List("ctl00%24pageContent%24subjectAreaDropDown", department),
+        List("ctl00%24pageContent%24searchButton.x", "0"),
+        List("ctl00%24pageContent%24searchButton.y", "0")
+      )
 
-    val bodyPairs = hiddenVals ++ inputVals
-    val form = bodyPairs.map(s => s"${s.head}=${URLEncoder.encode(s(1), UTF_8.toString)}").mkString("&")
+      val bodyPairs = hiddenVals ++ inputVals
+      val form = bodyPairs.map(s => s"${s.head}=${URLEncoder.encode(s(1), UTF_8.toString)}").mkString("&")
 
-    val basicPostResponse = basicCoursePageRequest.postData(form).asString
-    val resp = basicCoursePage(response.cookies).asString
-    resp
+      val basicPostResponse = basicCoursePageRequest.postData(form).asString
+      val resp = basicCoursePage(response.cookies).asString
+      resp
+    }) match {
+      case Success(v) => v
+      case Failure(e) =>
+        errorHandler(e.getMessage)
+        apply(quarter, department, response, errorHandler)
+    }
   }
 
   private def basicCoursePage(cookies: IndexedSeq[HttpCookie]): HttpRequest = {
