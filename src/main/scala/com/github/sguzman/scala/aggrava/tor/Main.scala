761bc8b5541da1ae91a58821888541c7c1ab9928
Salvador Guzman <guzmansalv@gmail.com>
2017-12-04 11:33:55 -0800

    simple json server

--- src/main/scala/com/github/sguzman/scala/aggrava/tor/Main.scala
+++ src/main/scala/com/github/sguzman/scala/aggrava/tor/Main.scala
@@ -2,6 +2,10 @@ package com.github.sguzman.scala.aggrava.tor
 
 import com.github.sguzman.scala.aggrava.tor.model.Model
 import com.github.sguzman.scala.aggrava.tor.retrieve.Retrieve
+import com.google.gson.GsonBuilder
+import lol.http._
+
+import scala.util.{Failure, Success}
 
 object Main {
   def main(args: Array[String]): Unit = {
@@ -10,6 +14,22 @@ object Main {
 
     val model = Model(raw)
     println(model)
+
+    val gson = new GsonBuilder().create
+    val json = gson.toJson()
+
+    val port = util.Try(System.getenv("PORT").toInt) match {
+      case Success(v) => v
+      case Failure(e) => 8888
+    }
+
+    println("Ready")
+    Server.listen(port) {
+      case GET at "/hello" =>
+        Ok(json)
+      case _ =>
+        NotFound
+    }
   }
 }
 
