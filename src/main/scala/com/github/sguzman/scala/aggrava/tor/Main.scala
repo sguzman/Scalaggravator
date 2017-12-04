c85b3e05b92a307a71324475121c3e170b221009
Salvador Guzman <guzmansalv@gmail.com>
2017-12-04 12:00:30 -0800

    moved filtering object to its own file

--- src/main/scala/com/github/sguzman/scala/aggrava/tor/Main.scala
+++ src/main/scala/com/github/sguzman/scala/aggrava/tor/Main.scala
@@ -1,6 +1,6 @@
 package com.github.sguzman.scala.aggrava.tor
 
-import com.github.sguzman.scala.aggrava.tor.model.{Model, Quarter}
+import com.github.sguzman.scala.aggrava.tor.model.Model
 import com.github.sguzman.scala.aggrava.tor.retrieve.Retrieve
 import com.google.gson.GsonBuilder
 import lol.http._
@@ -35,26 +35,4 @@ object Main {
         NotFound
     }
   }
-}
-
-object Filter {
-  private def byQuarter(model: List[Quarter], quarter: String): List[Quarter] =
-    model.filter(_.term.toLowerCase.contains(quarter.toLowerCase))
-
-  def byQuarterJson(model: List[Quarter], quarter: String): String =
-    new GsonBuilder().create.toJson(byQuarter(model, quarter).toArray)
-
-  private def byDepartment(model: List[Quarter], quarter: String, department: String): List[Quarter] =
-    byQuarter(model, quarter)
-      .filter(_.departments.department.toLowerCase.contains(department.toLowerCase))
-
-  def byDepartmentJson(model: List[Quarter], quarter: String, department: String): String =
-    new GsonBuilder().create.toJson(byDepartment(model, quarter, department).toArray)
-
-  private def byCourse(model: List[Quarter], quarter: String, department: String, courseName: String): List[Quarter] =
-    byDepartment(model, quarter, department)
-    .filter(_.departments.courses.course.toLowerCase.contains(courseName.toLowerCase))
-
-  def byCourseJson(model: List[Quarter], quarter: String, department: String, courseName: String): String =
-    new GsonBuilder().create.toJson(byCourse(model, quarter, department, courseName).toArray)
 }
